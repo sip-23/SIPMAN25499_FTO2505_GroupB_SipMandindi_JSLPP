49a5496611dc72a9e1bfd44f75fb8cb71111026e
sip-23 <ndiramandindi@gmail.com>
2025-08-14 04:53:07 +0200

    Corrected my light mode / dark mode styling
    To work on toggle next and finish of the the priority sorting of the task

--- scripts-pp.js
+++ scripts-pp.js
@@ -99,17 +99,17 @@ function createTaskElement(initialTasks) {
 
 
     if(initialTasks.priority === 'low') {
-        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
+        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white dark:bg-[#2B2C37] dark:text-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
         // Sets appropriate classes for styling and also creates a className for the new class
         taskElementCreated.innerHTML = `<h3 class="font-bold">${initialTasks.title}</h3><span class="flex justify-end inline h-[15px] w-[15px]">🟢</span>`;
         taskElementCreated.setAttribute('data-id', initialTasks.id);
     } else if (initialTasks.priority === 'medium') {
-        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
+        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white dark:bg-[#2B2C37] dark:text-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
         // Sets appropriate classes for styling and also creates a className for the new class
         taskElementCreated.innerHTML = `<h3 class="font-bold">${initialTasks.title} </h3><span class="flex justify-end inline h-[15px] w-[15px]">🟠</span>`;
         taskElementCreated.setAttribute('data-id', initialTasks.id);
     } else {
-        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
+        taskElementCreated.className = 'task flex mt-4 py-5 px-3 mr-2 w-max-[320px] xl:w-[280px] h-[60px] bg-white dark:bg-[#2B2C37] dark:text-white rounded-lg shadow-[0px_4px_6px_0px_rgba(54,78,126,0.1)] transition-all hover:shadow-md flex justify-between items-center';
         // Sets appropriate classes for styling and also creates a className for the new class
         taskElementCreated.innerHTML = `<h3 class="font-bold">${initialTasks.title} </h3><span class="flex justify-end inline h-[15px] w-[15px]">🔴</span>`;
         taskElementCreated.setAttribute('data-id', initialTasks.id);
